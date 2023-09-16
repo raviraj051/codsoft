e323e86a4467d20aefbb20836a5e79971c7a5d0e
Himanshu Chaurasiya <141658016+mrcoderji@users.noreply.github.com>
2023-09-16 15:24:37 +0530

    Update SimpleCalcualtor.py

--- SimpleCalcualtor.py
+++ SimpleCalcualtor.py
@@ -1,18 +1,12 @@
-# Python program for simple calculator
-
-# Function to add two numbers
 def add(num1, num2):
 	return num1 + num2
 
-# Function to subtract two numbers
 def subtract(num1, num2):
 	return num1 - num2
 
-# Function to multiply two numbers
 def multiply(num1, num2):
 	return num1 * num2
 
-# Function to divide two numbers
 def divide(num1, num2):
 	return num1 / num2
 
@@ -23,7 +17,6 @@ print("Please select operation -\n" \
 		"4. Divide\n")
 
 
-# Take input from the user
 select = int(input("Select operations form 1, 2, 3, 4 :"))
 
 number_1 = int(input("Enter first number: "))
